2d9c90f7bfb47445a344bafcb1740eb5117d6f4b
arthur.chen <efsn.chan@gmail.com>
2019-11-29 17:23:57 +0800

    add async file appender

--- src/main/resources/logback-spring.groovy
+++ src/main/resources/logback-spring.groovy
@@ -1,17 +1,43 @@
+import ch.qos.logback.classic.AsyncAppender
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.hook.DelayingShutdownHook
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.SizeAndTimeBasedFNATP
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 import kotlin.text.Charsets
 
 import static ch.qos.logback.classic.Level.INFO
 
-scan("60 seconds")
+scan("180 seconds")
 def LOG_PATTERN = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] [%X{uniqueTrackingNumber}] %-5level %logger{64} - %msg%n"
+
 appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "${LOG_PATTERN}"
         charset = Charsets.UTF_8
     }
 }
+
+appender("FILE", RollingFileAppender) {
+    rollingPolicy(TimeBasedRollingPolicy) {
+        fileNamePattern = "x.log.%d{yyyy-MM-dd}.%i.log"
+        maxHistory = 30
+        timeBasedFileNamingAndTriggeringPolicy(SizeAndTimeBasedFNATP) {
+            maxFileSize = "64MB"
+        }
+    }
+    encoder(PatternLayoutEncoder) {
+        pattern = "${LOG_PATTERN}"
+        charset = Charsets.UTF_8
+    }
+}
+
+appender("ASYNC", AsyncAppender) {
+    discardingThreshold = 0
+    queueSize = 256
+    appenderRef("FILE")
+}
+
 addShutdownHook { DelayingShutdownHook }
-root(INFO, ["CONSOLE"])
+root(INFO, ["CONSOLE", "ASYNC"])
