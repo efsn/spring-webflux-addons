bc54963528246777843fe8ae8c4dc730a4d560e5
arthur.chen <efsn.chan@gmail.com>
2019-11-29 17:39:52 +0800

    add filter to appender

--- src/main/resources/logback-spring.groovy
+++ src/main/resources/logback-spring.groovy
@@ -1,5 +1,7 @@
 import ch.qos.logback.classic.AsyncAppender
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.filter.LevelFilter
+import ch.qos.logback.classic.filter.ThresholdFilter
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.hook.DelayingShutdownHook
 import ch.qos.logback.core.rolling.RollingFileAppender
@@ -8,6 +10,7 @@ import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 import kotlin.text.Charsets
 
 import static ch.qos.logback.classic.Level.INFO
+import static ch.qos.logback.classic.Level.WARN
 
 scan("180 seconds")
 def LOG_PATTERN = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] [%X{uniqueTrackingNumber}] %-5level %logger{64} - %msg%n"
@@ -16,6 +19,11 @@ appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "${LOG_PATTERN}"
         charset = Charsets.UTF_8
+    }
+    filter(LevelFilter) {
+        level = WARN
+        onMatch = "ACCEPT"
+        onMismatch = "DENY"
     }
 }
 
@@ -30,6 +38,9 @@ appender("FILE", RollingFileAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "${LOG_PATTERN}"
         charset = Charsets.UTF_8
+    }
+    filter(ThresholdFilter) {
+        level = INFO
     }
 }
 
