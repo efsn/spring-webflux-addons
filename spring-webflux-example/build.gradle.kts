cc5159ca847c93f59e265e80d97dc06751a5ad3b
arthur.chen <efsn.chan@gmail.com>
2019-12-12 18:04:30 +0800

    add http client file

--- spring-webflux-example/build.gradle.kts
+++ spring-webflux-example/build.gradle.kts
@@ -1,7 +1,22 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+// repositories {
+//     maven {
+//         val githubUrl = project.findProperty("GITHUB_URI") as String? ?: System.getenv("GITHUB_URL")
+//         name = "githubPackages"
+//         url = uri("$githubUrl/${rootProject.name}")
+//         credentials {
+//             username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+//             password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+//         }
+//     }
+//     mavenCentral()
+//     jcenter()
+// }
+
 dependencies {
     implementation(project(":spring-webflux-addons-starter"))
+    // implementation("cn.elmi.spring.webflux:spring-webflux-addons-starter:1.0.0-snapshot")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.codehaus.groovy:groovy:2.5.7")
