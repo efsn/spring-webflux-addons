9bb2d16d03a5830155a948896c29517807f36159
arthur.chen <efsn.chan@gmail.com>
2020-08-28 15:52:29 +0800

    feat: web client add extension

--- spring-webflux-addons-starter/src/main/kotlin/cn/elmi/web/reactive/util/WebClientExtension.kt
+++ spring-webflux-addons-starter/src/main/kotlin/cn/elmi/web/reactive/util/WebClientExtension.kt
@@ -1,1 +1,16 @@
 package cn.elmi.web.reactive.util
+
+import cn.elmi.web.reactive.codec.LoggerDecoder
+import cn.elmi.web.reactive.codec.LoggerEncoder
+import org.slf4j.Logger
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.http.codec.json.Jackson2JsonEncoder
+import org.springframework.web.reactive.function.client.ExchangeStrategies
+import org.springframework.web.reactive.function.client.WebClient
+
+const val tenMb: Int = 10 * 1024 * 1024
+
+fun WebClient.Builder.logWith(logger: Logger) = exchangeStrategies(ExchangeStrategies.builder().codecs {
+    it.defaultCodecs().jackson2JsonEncoder(LoggerEncoder(Jackson2JsonEncoder(), logger))
+    it.defaultCodecs().jackson2JsonDecoder(LoggerDecoder(Jackson2JsonDecoder().apply { maxInMemorySize = tenMb }, logger))
+}.build())
